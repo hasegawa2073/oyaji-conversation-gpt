df9a37b808f94c02ace224a55a13b95d473bb228
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-04-09 22:32:47 +0900

    feat: opg 設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import './globals.css';
 export const metadata = {
   title: '居酒屋の会話GPT',
   description: '',
+  images: [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/opg.png`,
+      width: 1440,
+      height: 940,
+    },
+  ],
   icons: {
     icon: '/icon.png',
   },

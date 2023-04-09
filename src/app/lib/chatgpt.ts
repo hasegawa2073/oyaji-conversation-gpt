54fbe81ba25cb2c2e139da1ad7e941251bde071f
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-04-09 22:22:51 +0900

    feat: おやじ2人を会話させる

---
+++ src/app/lib/chatgpt.ts
@@ -0,0 +1,28 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const chatGPT = async (prompt: string): Promise<string | null> => {
+  const apiKey = process.env.OPENAI_API_KEY;
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${apiKey}`,
+  };
+  const data = {
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: prompt }],
+    max_tokens: 140,
+  };
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data),
+    });
+    const json = await response.json();
+    return json.choices[0].message.content;
+  } catch (error) {
+    console.error('Error fetching ChatGPT response:', error);
+    return null;
+  }
+};
+
+export default chatGPT;

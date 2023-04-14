67b5ec2ea131bbd32cc1f3522b90a7589fb2e56e
hasegawa2073 <hasegawatatsuya206@gmail.com>
2023-04-15 05:16:10 +0900

    fix: レスポンス待ち ボタンを非活性

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,21 @@
 type Props = {
   label?: string;
   bgColor?: string;
+  isFetching?: boolean;
 };
 
-export default function Button({ label = '仲直りさせる', bgColor = 'bg-blue-500' }: Props) {
+export default function Button({
+  label = '仲直りさせる',
+  bgColor = 'bg-blue-500',
+  isFetching,
+}: Props) {
   return (
-    <button className={`px-20 py-5 text-2xl font-bold text-white -sm:px-10 -sm:py-4 ${bgColor}`}>
+    <button
+      className={`px-20 py-5 text-2xl font-bold text-white -sm:px-10 -sm:py-4 ${bgColor} ${
+        isFetching && 'cursor-wait'
+      }`}
+      disabled={isFetching}
+    >
       {label}
     </button>
   );
